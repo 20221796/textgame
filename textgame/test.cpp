f4fbf04ddec9412732df3d25c8d6d741e6c541f2
tjwlsqo44 <tjwlsqo44@gmail.com>
2022-12-03 21:44:31 +0900

    던전 외 요소 개발 완료

--- textgame/test.cpp
+++ textgame/test.cpp
@@ -1,11 +1,36 @@
-////test case code
+//test case code
 // 
 //#include <stdio.h>
+//#include <stdlib.h>
+//#include <time.h>
 //
 //#define and &&
 //
+//int level = 5;
+//
+//void a(void);
+//
+//
 //int main(void)
 //{
-//	 int menu = 0;
-//	 do	scanf("%d", &menu); while (1 <= menu and menu <= 4);
-// }
+//	srand(time(0));
+//	a();
+//	return 0;
+//}
+//
+//void a(void)
+//{
+//	int scnt = 0;
+//	int fcnt = 0;
+//
+//	for (int i = 0; i < 1000; i++)
+//	{
+//		int success_rate = rand() % level + 1; //·¹º§ÀÌ 1 ¿À¸¦ ¶§¸¶´Ù °­È­ È®·üÀ» ³·Ãã
+//
+//		if (success_rate == level) scnt++;
+//		else fcnt++;
+//	}
+//
+//	printf("¼º°ø È®·ü: %.1f% \n", float(scnt)/10);
+//	printf("½ÇÆÐ È®·ü: %.1f% \n", float(fcnt)/10);
+//}
