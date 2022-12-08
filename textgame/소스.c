861892bdfd689c3160cc724e34e9c476c5939e93
20221796 <dorisjbssu@gmail.com>
2022-12-08 13:08:00 +0900

    던전 출입 함수 생성

--- textgame/소스.c
+++ textgame/소스.c
@@ -15,11 +15,31 @@
 #define or ||
 
 //게임에 활용될 전역변수
-char name[30] = "무명의 모험가";
-int level = 1;
-int hp = level * 1000;
-int ad = level * 100;
-int gold = 100;
+
+typedef struct 
+{
+	char name[30];
+	int level;
+	int hp;
+	int ad;
+	int gold;
+} Player;
+
+Player player ={ "무명의 모험가", 1, 1000, 100, 100 };
+
+typedef struct
+{
+	char name[30];
+	int level;
+	int hp;
+	int ad;
+} Monster;
+
+Monster boss;
+Monster monster;
+
+
+int is_boss = false;
 
 //게임에 활용될 함수
 void intro(void); //게임 시작 전 인트로 띄우고 이름 입력받기
@@ -30,6 +50,8 @@ void print_profile(void); //플레이어 정보를 맨 윗줄에 띄움
 
 void goto_dungeon(void); //던전으로 이동
 int summon_monster(int floor); //몬스터 소환
+void summon_boss(int floor);
+void summon_common(int floor);
 void start_battle(void); //전투 시작
 
 void goto_smithy(void); //대장간으로 이동
@@ -69,14 +91,15 @@ int main(void)
 
 
 
+
 void intro(void) //시작화면을 출력하는 함수
 {
 	title();
 
 	printf("이름을 입력해 주세요 : ");
-	scanf("%s", name);
+	scanf("%s", player.name);
 
-	printf("\n%s님 환영합니다. \n아무 키나 누르면 게임이 시작됩니다. ", name);
+	printf("\n%s님 환영합니다. \n아무 키나 누르면 게임이 시작됩니다. ", player.name);
 	getch();
 }
 
@@ -147,10 +170,10 @@ void display_menu(void) //메인메뉴를 출력하는 함수
 
 void print_profile(void) //캐릭터의 상태를 보여주는 함수
 {
-	hp = level * 1000;
-	ad = level * 100;
-	printf("이름 : %s \n", name);
-	printf("Lv%d  체력: %d  공격력: %d  보유 골드: %d \n\n", level, hp, ad, gold);
+	player.hp = player.level * 1000;
+	player.ad = player.level * 100;
+	printf("이름 : %s \n", player.name);
+	printf("Lv%d  체력: %d  공격력: %d  보유 골드: %d \n\n", player.level, player.hp, player.ad, player.gold);
 	//printf("치명타 확률 : %f \n\n"); //추후 구현
 }
 
@@ -243,8 +266,8 @@ void goto_shop(void) //상점으로 이동하는 함수
 
 void charge_cash() //골드 충전 함수
 {
-	printf("\n10000골드를 충전했습니다\t 골드 %d -> %d \n", gold, gold + 10000);
-	gold += 10000;
+	printf("\n10000골드를 충전했습니다\t 골드 %d -> %d \n", player.gold, player.gold + 10000);
+	player.gold += 10000;
 }
 
 void goto_smithy(void) //대장간으로 이동하는 함수
@@ -293,8 +316,8 @@ void goto_smithy(void) //대장간으로 이동하는 함수
 void upgrade_equipment(void) //장비 업그레이드 함수
 {
 	Sleep(200);
-	if (level >= 99) printf("\n최대 레벨입니다 \n");
-	else if (gold - 100 * level < 0)
+	if (player.level >= 99) printf("\n최대 레벨입니다 \n");
+	else if (player.gold - 100 * player.level < 0)
 	{
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
 		printf("\n대장장이");
@@ -303,15 +326,15 @@ void upgrade_equipment(void) //장비 업그레이드 함수
 	}
 	else
 	{
-		printf("\n골드를 소모합니다\t 골드 %d -> %d \n", gold, gold - 100 * level);
-		gold -= 100 * level;
-		int success_rate = rand() % level + 1; //레벨이 1 오를 때마다 강화 확률을 낮춤
+		printf("\n골드를 소모합니다\t 골드 %d -> %d \n", player.gold, player.gold - 100 * player.level);
+		player.gold -= 100 * player.level;
+		int success_rate = rand() % player.level + 1; //레벨이 1 오를 때마다 강화 확률을 낮춤
 
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
 		Sleep(500);
 		printf("투쾅 투쾅 투쾅!\n");
 
-		if (success_rate == level) //(1÷레벨)의 확률로 강화가 성공하도록 설정
+		if (success_rate == player.level || success_rate == player.level / 2) //(1÷레벨)의 확률로 강화가 성공하도록 설정
 		{
 			Sleep(500);
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
@@ -319,9 +342,9 @@ void upgrade_equipment(void) //장비 업그레이드 함수
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 			printf(" : 음하하핳하 나같은 대장장이를 만난 걸 행운으로 알라고 친구!\n\n");
 			Sleep(500);
-			printf("(장비 제련에 성공했습니다  레벨 %d -> %d) \n", level, level + 1);
+			printf("(장비 제련에 성공했습니다  레벨 %d -> %d) \n", player.level, player.level + 1);
 
-			level += 1;
+			player.level += 1;
 		}
 		else
 		{
@@ -372,6 +395,7 @@ void goto_dungeon(void) //던전에 입장하는 함수
 		else if (menu != 2) printf("몬스터에게 겁에 질려 판단력이 흐려졌군요... 선택지는 1과 2뿐입니다. \n\n");
 	} while (menu != 2);
 
+	
 	Sleep(500);
 	printf("던전에서 탈출합니다... \n");
 	Sleep(500);
@@ -381,6 +405,8 @@ void goto_dungeon(void) //던전에 입장하는 함수
 
 int summon_monster(int floor) //층수에 맞는 몬스터를 설정하는 함수
 {
+	is_boss = true;
+
 	if (floor > 99) return false; //99층 이상이면 몬스터를 소환하지 않음
 	else
 	{
@@ -392,16 +418,101 @@ int summon_monster(int floor) //층수에 맞는 몬스터를 설정하는 함수
 
 void summon_boss(int floor)
 {
+	is_boss = true;
+	boss.level = floor;
+	boss.hp = floor * 800;
+	boss.ad = floor * 50;
+
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
 	Sleep(500);
 	printf("[경고] 지하 %d층까지 와 보스 몬스터가 출현합니다. \n", floor);
 	printf("보스 몬스터와의 전투에서는 중간에 도망가는 것이 불가능하며 죽으면 레벨이 1이 됩니다.");
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+
+	switch (floor / 10)
+	{
+	case 0:
+		strcpy(boss.name, "거대 슬라임");
+		break;
+	case 1:
+		strcpy(boss.name, "키메라 엔트");
+		break;
+	case 2:
+		strcpy(boss.name, "엘리스");
+		break;
+	case 3:
+		strcpy(boss.name, "하이 워울프");
+		break;
+	case 4:
+		strcpy(boss.name, "리자드킹");
+		break;
+	case 5:
+		strcpy(boss.name, "미노타우르스");
+		break;
+	case 6:
+		strcpy(boss.name, "하이 언데드");
+		break;
+	case 7:
+		strcpy(boss.name, "셰이드");
+		break;
+	case 8:
+		strcpy(boss.name, "가디언 골렘");
+		break;
+	case 9:
+		strcpy(boss.name, "츄파");
+		break;
+	}
+	
+	printf("지하 %d 층 보스 %s가 출현합니다. ", floor, boss.name);
 }
 
 void summon_common(int floor)
 {
-	printf("지하 %d 층 몬스터 %s가 출현합니다. ");
+	is_boss = false;
+	monster.level = floor;
+	monster.hp = floor * 300;
+	monster.ad = floor * 30;
+
+	switch (floor / 10)
+	{
+	case 0:
+		strcpy(monster.name, "슬라임");
+		break;
+	case 1:
+		strcpy(monster.name, "병정 개미");
+		break;
+	case 2:
+		strcpy(monster.name, "엘리스");
+		break;
+	case 3:
+		strcpy(monster.name, "회색 늑대");
+		break;
+	case 4:
+		strcpy(monster.name, "리자드맨");
+		break;
+	case 5:
+		strcpy(monster.name, "타우");
+		break;
+	case 6:
+		strcpy(monster.name, "스켈레톤");
+		break;
+	case 7:
+		strcpy(monster.name, "레이스");
+		break;
+	case 8:
+		strcpy(monster.name, "골렘");
+		break;
+	case 9:
+		strcpy(monster.name, "이무기");
+		break;
+	}
+	
+	printf("지하 %d 층 몬스터 %s가 출현합니다. ", floor, monster.name);
+}
+
+void start_battle(void)
+{
+	player.hp -= monster.ad;
 }
 
 void printT(int x, int y)		//T 출력
